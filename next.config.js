bf01828814907b29a9ca854d308744a88df9f1e9
Zcodming <zahwatri.riyanto@gmail.com>
2024-07-20 04:07:25 +0700

    next config file

--- next.config.js
+++ next.config.js
@@ -3,7 +3,23 @@ const nextConfig = {
 	experimental: {
 		serverComponentsExternalPackages: ["@prisma/client", "bcrypt"],
 	},
+<<<<<<< HEAD
+=======
 	// output: { export: any },
+>>>>>>> 8bb988241466d87e8bfed0942d94c1adced58245
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
